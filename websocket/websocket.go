3d399846fb14a213b32102cad78d244dfd54bdb2
inhere <in.798@qq.com>
2018-12-24 10:03:14 +0800

    update

--- websocket/websocket.go
+++ websocket/websocket.go
@@ -1,7 +1,21 @@
 // Package websocket utils
 package websocket
 
+import "github.com/gorilla/websocket"
+
 // Refer links:
 //
+// gorilla/websocket https://github.com/gorilla/websocket
 // golang.org/x/net/websocket
 // golang.org/x/tools/playground/socket/socket.go
+
+// WSApp struct
+type WSApp struct {
+	// all connections
+	clients map[string]*websocket.Conn
+}
+
+// NewWSApp create new WSApp
+func NewWSApp() *WSApp {
+	return &WSApp{}
+}
