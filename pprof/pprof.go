7815553bd033baa636a1684d29e3dbe624b0b65c
Matt Ma <395241021@qq.com>
2019-08-30 14:19:06 +0800

    update pprof

--- pprof/pprof.go
+++ pprof/pprof.go
@@ -7,17 +7,28 @@ import (
 
 // UsePProf enable for the router
 func UsePProf(r *rux.Router) {
-	r.GET("/debug/pprof/", func(c *rux.Context) {
-		c.Resp.Header().Set(rux.ContentType, "text/html; charset=utf-8")
+	routers := []struct {
+		Method  string
+		Path    string
+		Handler rux.HandlerFunc
+	}{
+		{rux.GET, "/", rux.WrapHTTPHandlerFunc(pprof.Index)},
+		{rux.GET, "/heap", rux.WrapHTTPHandler(pprof.Handler("heap"))},
+		{rux.GET, "/goroutine", rux.WrapHTTPHandler(pprof.Handler("goroutine"))},
+		{rux.GET, "/allocs", rux.WrapHTTPHandler(pprof.Handler("allocs"))},
+		{rux.GET, "/block", rux.WrapHTTPHandler(pprof.Handler("block"))},
+		{rux.GET, "/threadcreate", rux.WrapHTTPHandler(pprof.Handler("threadcreate"))},
+		{rux.GET, "/cmdline", rux.WrapHTTPHandlerFunc(pprof.Cmdline)},
+		{rux.GET, "/profile", rux.WrapHTTPHandlerFunc(pprof.Profile)},
+		{rux.GET, "/symbol", rux.WrapHTTPHandlerFunc(pprof.Symbol)},
+		{rux.POST, "/symbol", rux.WrapHTTPHandlerFunc(pprof.Symbol)},
+		{rux.GET, "/trace", rux.WrapHTTPHandlerFunc(pprof.Trace)},
+		{rux.GET, "/mutex", rux.WrapHTTPHandler(pprof.Handler("mutex"))},
+	}
 
-		pprof.Index(c.Resp, c.Req)
+	r.Group("/debug/pprof", func() {
+		for _, route := range routers {
+			r.Add(route.Path, route.Handler, route.Method)
+		}
 	})
-	r.GET("/debug/pprof/heap", rux.WrapHTTPHandler(pprof.Handler("heap")))
-	r.GET("/debug/pprof/goroutine", rux.WrapHTTPHandler(pprof.Handler("goroutine")))
-	r.GET("/debug/pprof/block", rux.WrapHTTPHandler(pprof.Handler("block")))
-	r.GET("/debug/pprof/threadcreate", rux.WrapHTTPHandler(pprof.Handler("threadcreate")))
-	r.GET("/debug/pprof/cmdline", rux.WrapHTTPHandlerFunc(pprof.Cmdline))
-	r.GET("/debug/pprof/profile", rux.WrapHTTPHandlerFunc(pprof.Profile))
-	r.GET("/debug/pprof/symbol", rux.WrapHTTPHandlerFunc(pprof.Symbol))
-	r.GET("/debug/pprof/mutex", rux.WrapHTTPHandler(pprof.Handler("mutex")))
 }
