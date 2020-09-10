6974e6d4a99951ba6a16c7a5fb0b3d4bfaa56cf0
inhere <in.798@qq.com>
2020-09-10 17:03:12 +0800

    update some new binding methods

--- context_binding.go
+++ context_binding.go
@@ -5,7 +5,7 @@ import "github.com/gookit/rux/binding"
 // ShouldBind bind request data to an struct, will auto call validator
 //
 // Usage:
-//	err := c.ShouldBind(u, binding.JSON)
+//	err := c.ShouldBind(&user, binding.JSON)
 func (c *Context) ShouldBind(obj interface{}, binder binding.Binder) error {
 	return binder.Bind(c.Req, obj)
 }
@@ -21,6 +21,32 @@ func (c *Context) MustBind(obj interface{}, binder binding.Binder) {
 	}
 }
 
+// AutoBind auto bind request data to an struct, will auto select binding.Binder by content-type
+//
+// Usage:
+//	err := c.AutoBind(&user)
+func (c *Context) AutoBind(obj interface{}) error {
+	return binding.Auto(c.Req, obj)
+}
+
 /*************************************************************
- * context data binding
+ * quick context data binding
  *************************************************************/
+
+// BindJSON request data to an struct, will auto call validator
+//
+// Usage:
+//	err := c.BindJSON(&user)
+//
+func (c *Context) BindJSON(obj interface{}) error {
+	return binding.JSON.Bind(c.Req, obj)
+}
+
+// BindXML request data to an struct, will auto call validator
+//
+// Usage:
+//	err := c.BindXML(&user)
+//
+func (c *Context) BindXML(obj interface{}) error {
+	return binding.XML.Bind(c.Req, obj)
+}
