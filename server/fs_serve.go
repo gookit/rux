f9520351eda8e3cef0ebb700a8b0fa15baa51f47
inhere <in.798@qq.com>
2025-07-31 14:15:16 +0800

    :necktie: up: update builtin server create logic

---
+++ server/fs_serve.go
@@ -0,0 +1,9 @@
+package server
+
+// NewFsServer creates a new Server provide fs serve.
+func NewFsServer() *Server {
+	s := New(false)
+
+	// TODO
+	return s
+}
--- server/server.go
+++ server/server.go
@@ -3,7 +3,10 @@ package server
 import (
 	"fmt"
 
+	"github.com/gookit/color/colorp"
+	"github.com/gookit/goutil/strutil"
 	"github.com/gookit/rux"
+	"github.com/gookit/rux/pkg/handlers"
 )
 
 // Server struct
@@ -14,19 +17,43 @@ type Server struct {
 
 	// Host server host. eg: localhost, 127.0.0.1
 	Host string
-	Port int
+	Port uint
 }
 
 // New server instance
-func New() *Server {
-	return &Server{
-		Router: rux.New(),
+func New(debugMode bool) *Server {
+	rux.Debug(debugMode)
+	r := rux.New(rux.EnableCaching)
+
+	r.Use(handlers.PanicsHandler())
+	if debugMode {
+		r.Use(handlers.RequestLogger())
 	}
+
+	// handle error
+	r.OnError = func(c *rux.Context) {
+		if err := c.FirstError(); err != nil {
+			colorp.Errorln(err)
+			c.HTTPError(err.Error(), 400)
+			return
+		}
+	}
+
+	return &Server{Router: r}
+}
+
+// SetHostPort set host and port
+func (s *Server) SetHostPort(host string, port uint) {
+	s.Host = host
+	s.Port = port
 }
 
 // Start server with addr
-func (s *Server) Start(addr string) {
-	s.Listen(addr)
+func (s *Server) Start() {
+	portStr := strutil.SafeString(s.Port)
+	// addr := s.Host + ":" + portStr
+	// s.Listen(addr)
+	s.Listen(s.Host, portStr)
 }
 
 // String get string
