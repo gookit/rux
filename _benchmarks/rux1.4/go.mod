f4b9d099463b020190c657b4744fe5dd28b164b7
inhere <in.798@qq.com>
2026-03-02 20:20:12 +0800

    chore: Organize benchmarks and example deployment files
    Rename rux1.2-stable and rux1.3-stable to rux1.2 and rux1.3.
    Add rux1.4 benchmark (go.mod, go.sum, main.go).
    Move build Dockerfiles and nginx-vhost.conf into _examples/deploy.

---
+++ _benchmarks/rux1.4/go.mod
@@ -0,0 +1,5 @@
+module rux14-stable
+
+go 1.21
+
+require github.com/gookit/rux v1.4.1
---
+++ _benchmarks/rux1.4/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gookit/rux"
+)
+
+// run serve:
+// 	go run ./rux-stable/main.go
+// bench test:
+// 	bombardier -c 125 -n 1000000 http://localhost:3000
+// 	bombardier -c 125 -n 1000000 http://localhost:3000/user/42
+func main() {
+	// close debug
+	r := rux.New()
+	// r := rux.New(rux.EnableCaching)
+
+	r.GET("/", func(c *rux.Context) {
+		_, _ = c.Resp.Write([]byte("Welcome!\n"))
+	})
+
+	r.GET("/user/{id}", func(c *rux.Context) {
+		c.WriteString(c.Param("id"))
+	})
+
+	fmt.Println("Server started at localhost:3000")
+
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		log.Fatal(err)
+	}
+}
