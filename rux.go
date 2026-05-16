ec4721906a780810383f817c987815cfa64826a8
inhere <in.798@qq.com>
2026-05-16 10:44:30 +0800

    feat(v2): add methodIndex to rux.go (P-1)

--- rux.go
+++ rux.go
@@ -1,7 +1,8 @@
-// Package rux is a simple and fast request router for golang HTTP applications.
+// Package rux is a high-performance HTTP router for Go.
 //
 // Source code and other details for the project are available at GitHub:
-// 		https://github.com/gookit/rux
+//
+//	https://github.com/gookit/rux
 //
 // Usage please ref examples and README
 package rux
@@ -24,6 +25,54 @@ const (
 	CONNECT = "CONNECT"
 	OPTIONS = "OPTIONS"
 )
+
+// methodCount is the total number of HTTP methods rux understands.
+const methodCount = 9
+
+// methodIndex maps an HTTP method string to a 0..8 array index.
+// Returns -1 for unknown methods.
+func methodIndex(m string) int {
+	if len(m) == 0 {
+		return -1
+	}
+	switch m[0] {
+	case 'G':
+		if m == GET {
+			return 0
+		}
+	case 'H':
+		if m == HEAD {
+			return 1
+		}
+	case 'P':
+		if m == POST {
+			return 2
+		}
+		if m == PUT {
+			return 3
+		}
+		if m == PATCH {
+			return 4
+		}
+	case 'D':
+		if m == DELETE {
+			return 5
+		}
+	case 'O':
+		if m == OPTIONS {
+			return 6
+		}
+	case 'C':
+		if m == CONNECT {
+			return 7
+		}
+	case 'T':
+		if m == TRACE {
+			return 8
+		}
+	}
+	return -1
+}
 
 const (
 	// ContentType header key
